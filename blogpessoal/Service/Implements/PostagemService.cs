ca2139c261029a1f538a61d2cb183d02d47ec731
GasparLeonardi <gasparlleonardi@gmail.com>
2023-09-25 15:33:03 -0300

    Projeto Blog Pessoal Crud 02

--- blogpessoal/Service/Implements/PostagemService.cs
+++ blogpessoal/Service/Implements/PostagemService.cs
@@ -14,34 +14,57 @@ namespace blogpessoal.Service.Implements
             _context = context;
         }
 
-        public Task<Postagem?> Create(Postagem postagem)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Delete(Postagem postagem)
-        {
-            throw new NotImplementedException();
-        }
-
         public async Task<IEnumerable<Postagem>> GetAll()
         {
             return await _context.Postagens.ToListAsync();
         }
 
-        public Task<Postagem?> GetById(long id)
+        public async Task<Postagem?> GetById(long id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var Postagem = await _context.Postagens.FirstAsync(i => i.Id == id);
+                return Postagem;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public async Task<IEnumerable<Postagem>> GetByTitulo(string titulo)
+        {
+            var Postagem = await _context.Postagens
+                                .Where(p => p.Titulo.Contains(titulo))
+                                .ToListAsync();
+            return Postagem;
+        }
+        public async Task<Postagem?> Create(Postagem postagem)
+        {
+            await _context.Postagens.AddAsync(postagem);
+            await _context.SaveChangesAsync();
+
+            return postagem;
+        }
+        public async Task<Postagem?> Update(Postagem postagem)
+        {
+            var PostagemUpdate = await _context.Postagens.FindAsync(postagem.Id);
+
+            if (PostagemUpdate is null)
+            {
+                return null;
+            }
+            _context.Entry(PostagemUpdate).State = EntityState.Detached;
+            _context.Entry(postagem).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return postagem;
         }
 
-        public Task<IEnumerable<Postagem>> GetByTitulo(string titulo)
+        public async Task Delete(Postagem postagem)
         {
-            throw new NotImplementedException();
-        }
+            _context.Remove(postagem);
+            await _context.SaveChangesAsync();
 
-        public Task<Postagem?> Update(Postagem postagem)
-        {
-            throw new NotImplementedException();
         }
     }
 }
